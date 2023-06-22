f5d2fa105fe6b1f5a6b49d132587e0a4dc795de3
sinjinxjohn <sinjin.hotlinebling@gmail.com>
2023-06-22 11:13:49 +0530

    timer added

--- application/bereal/lib/loginPage.dart
+++ application/bereal/lib/loginPage.dart
@@ -7,6 +7,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 
+import 'homePage.dart';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -20,58 +22,131 @@ class _LoginPageState extends State<LoginPage> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.black,
-        body: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 100,
-            ),
-            Text(
-              "BeReal.",
-              style: GoogleFonts.lato(
-                  fontSize: 45,
-                  fontWeight: FontWeight.w800,
-                  color: Colors.white),
-            ),
-            Text(
-              "Where you can be the real you",
-              style: GoogleFonts.lato(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w800,
-                  color: Colors.grey),
-            ),
-            SizedBox(
-              height: 300,
-            ),
-            // Text(
-            //   'Click on the button to sign in with Google',
-            //   style: GoogleFonts.lato(
-            //       fontSize: 20,
-            //       fontWeight: FontWeight.w800,
-            //       color: Colors.white),
-            // ),
-            InkWell(
-              onTap: () {
-                signInWithGoogle();
-              },
-              child: Center(
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white),
-                  height: 50,
-                  width: 200,
-                  child: Center(
-                      child: Text(
-                    'Sign In With Google',
-                    style: GoogleFonts.lato(
-                        fontSize: 17, fontWeight: FontWeight.w700),
-                  )),
-                  // color: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 100,
+              ),
+              Text(
+                "BeReal.",
+                style: GoogleFonts.lato(
+                    fontSize: 45,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.white),
+              ),
+              Text(
+                "Where you can be the real you",
+                style: GoogleFonts.lato(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.grey),
+              ),
+              const SizedBox(
+                height: 250,
+              ),
+             Padding(
+                padding: EdgeInsets.only(left: 40,right: 40),
+                child: TextField(
+                
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    
+                    filled: true,
+                    fillColor: Colors.black,
+                    focusColor: Colors.grey,
+                    contentPadding: EdgeInsets.symmetric(vertical: 9,horizontal: 30),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 2, color: Colors.white,)),
+                      labelText: "Email",
+                      labelStyle:GoogleFonts.lato(color: Colors.white) ,
+                      
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                       border: InputBorder.none),
                 ),
               ),
-            )
-          ],
+              const SizedBox(height: 15,),
+               Padding(
+                padding: EdgeInsets.only(left: 40,right: 40),
+                child: TextField(
+                
+                
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                  
+                    filled: true,
+                    fillColor: Colors.black,
+                    contentPadding: const EdgeInsets.symmetric(vertical: 9,horizontal: 30),
+                      enabledBorder: const OutlineInputBorder(
+                        
+                        
+                        
+                          borderSide:
+                              BorderSide(width: 2, color: Colors.white,)),
+                      labelText: "Password",
+                      labelStyle: GoogleFonts.lato(color: Colors.white),
+                      
+
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                    border: InputBorder.none
+                      ),
+                ),
+              ),
+        
+              
+            
+             
+              SizedBox(height: 10,),
+              InkWell(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+                  // signInWithGoogle();
+                },
+                child: Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color:Colors.white),
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.black),
+                    height: 50,
+                    width: 200,
+                    child: Center(
+                        child: Text(
+                      'Login In',
+                      style: GoogleFonts.lato(color: Colors.white,
+                          fontSize: 17, fontWeight: FontWeight.w700),
+                    )),
+                    // color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(height: 5,),
+              InkWell(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+                  // signInWithGoogle();
+                },
+                child: Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white),
+                    height: 50,
+                    width: 200,
+                    child: Center(
+                        child: Text(
+                      'Sign In With Google',
+                      style: GoogleFonts.lato(
+                          fontSize: 17, fontWeight: FontWeight.w700),
+                    )),
+                    // color: Colors.white,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- application/bereal/lib/main.dart
+++ application/bereal/lib/main.dart
@@ -3,12 +3,16 @@ import 'package:bereal/homePage.dart';
 import 'package:bereal/loginPage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print(fcmToken);
 
   runApp(const MyApp());
 }
@@ -34,20 +38,22 @@ class MyApp extends StatelessWidget {
           // is not restarted.
           primarySwatch: Colors.blue,
         ),
-        home: StreamBuilder<User?>(
-            stream: FirebaseAuth.instance.authStateChanges(),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return Text(snapshot.error.toString());
-              }
-              if (snapshot.connectionState == ConnectionState.active) {
-                if (snapshot.data == null) {
-                  return LoginPage();
-                } else {
-                  return HomePage();
-                }
-              }
-              return CircularProgressIndicator();
-            }));
+        home: LoginPage()
+        // StreamBuilder<User?>(
+        //     stream: FirebaseAuth.instance.authStateChanges(),
+        //     builder: (context, snapshot) {
+        //       if (snapshot.hasError) {
+        //         return Text(snapshot.error.toString());
+        //       }
+        //       if (snapshot.connectionState == ConnectionState.active) {
+        //         if (snapshot.data == null) {
+        //           return LoginPage();
+        //         } else {
+        //           return HomePage();
+        //         }
+        //       }
+        //       return CircularProgressIndicator();
+        //     })
+            );
   }
 }
